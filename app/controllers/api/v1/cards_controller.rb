f875c8020d9b24c2a3813330fd17f3e84cd13923
swdecker <swdecker@gmail.com>
2019-12-16 10:13:06 -0600

    check the card database before hitting hte card api

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,28 +1,36 @@
 class Api::V1::CardsController < ApplicationController
     def search
-        puts "searching for card #{card_params[:cardName]}"
-        cards = MTG::Card.where(name: card_params[:cardName]).all
-        # card = MTG::Card.find(88803)
-        if cards[0] && cards[0].name == card_params[:cardName]
-            puts '************'
-            puts cards[0]
-            puts '************'
-        end
-        if cards && cards.length > 0 
-            i = 0
-            while i < cards.length 
-                if cards[i].name == card_params[:cardName] && cards[i].image_url
-                    card = Card.find_or_create_by(name: cards[i].name)
-                    card.image_url ||= cards[i].image_url
-                    card.save
-                    render json: {card: {
-                        name:card.name,
-                        image_url: card.image_url
-                        }}, status: :accepted
-                    break
+        card_name = card_params[:cardName]
+        puts "searching for card #{card_name}"
+        
+
+        # check db first before sending fetch
+        card = Card.find_by(name: card_name)
+        # unless the card is in the database, fetch for the card by name
+
+        unless card 
+            cards = MTG::Card.where(name: card_params[:cardName]).all
+            
+            if cards && cards.length > 0 
+                i = 0
+                while i < cards.length 
+                    # if more fields are added to card, need to update this check
+                    # should also reset database so that full data will be stored
+                    if cards[i].name == card_params[:cardName] && cards[i].image_url
+                        card = Card.new(name: cards[i].name)
+                        card.image_url = cards[i].image_url
+                        card.save
+                        break
+                    end
+                    i+=1
                 end
-                i+=1
             end
+        end
+        if card
+            render json: {card: {
+                            name:card.name,
+                            image_url: card.image_url
+                            }}, status: :accepted
         else
             render json: {message: 'Card not found'}, status: :no_content
         end
